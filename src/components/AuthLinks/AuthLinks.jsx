b80ebe443694553a6407d69cba0f07f1dca90f2a
shohan <dev.shohanur@gmail.com>
2024-05-02 22:14:45 +0600

    nav bar responsive done

--- src/components/AuthLinks/AuthLinks.jsx
+++ src/components/AuthLinks/AuthLinks.jsx
@@ -1,12 +1,47 @@
+"use client"
 
-import style from'./AuthLinks.module.css'
+import Link from "next/link";
+import styles from "./AuthLinks.module.css";
+import { useState } from "react";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false)
+  const status = "unauthenticated";
+
   return (
-    <div className={style.title}>AuthLinks
+    <>
+      {status === "authenticated" ? (
+        <>
+          <Link href="/write" className={styles.link}>Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      ) : (
+        <Link href="/login" className={styles.link}>Login</Link>
+      )}
 
-    </div>
-  )
-}
+      <div className={styles.burger} onClick={()=>setOpen(!open)} >
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
 
-export default AuthLinks
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">Content</Link>
+          <Link href="/">About</Link>
+          {status === "authenticated" ? (
+        <>
+          <Link href="/write" >Write</Link>
+          <span className={styles.link}>Logout</span>
+        </>
+      ) : (
+        <Link href="/login">Login</Link>
+      )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,35 @@
-import Link from 'next/link'
-import AuthLinks from '../AuthLinks/AuthLinks'
-import ThemeToggle from '../ThemeToggle/ThemeToggle'
-import styles from'./navbar.module.css'
-import Image from 'next/image'
+import Link from "next/link";
+import AuthLinks from "../AuthLinks/AuthLinks";
+import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import styles from "./navbar.module.css";
+import Image from "next/image";
 const Navbar = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.social}>
-            <Image src={'/facebook.png'} alt='facebook' width={24} height={24}/>
-            <Image src={'/instagram.png'} alt='instagram' width={24} height={24}/>
-            <Image src={'/tiktok.png'} alt='tiktok' width={24} height={24}/>
-            <Image src={'/youtube.png'} alt='youtube' width={24} height={24}/>
-        </div>
-        <div className={styles.logo}>
-            <h1>Virus's Blog</h1>
-        </div>
-        <div className={styles.links}>
-            <ThemeToggle/>
-                <Link href="/" >HomePage</Link>
-                <Link href="/" >Content</Link>
-                <Link href="/" >About</Link>
-            <AuthLinks/>
-        </div>
+      <div className={styles.social}>
+        <Image src={"/facebook.png"} alt="facebook" width={24} height={24} />
+        <Image src={"/instagram.png"} alt="instagram" width={24} height={24} />
+        <Image src={"/tiktok.png"} alt="tiktok" width={24} height={24} />
+        <Image src={"/youtube.png"} alt="youtube" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>
+        <h1>Virus's Blog</h1>
+      </div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          HomePage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Content
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <AuthLinks />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
